852fda87bd5683811cc720bcad1cd34850da4e79
Cameron Cunning <negatifzeo@gmail.com>
2023-03-29 20:00:42 -0500

    Rust tools, chat gpt, vim-surround

---
+++ plugins/chatgpt.lua
@@ -0,0 +1,13 @@
+return {
+
+
+  "jackMort/ChatGPT.nvim",
+  lazy = false,
+  dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
+  },
+  opts = {}
+
+}
